d20fbfe329a20e48708d78c396b36d9bb5401bd3
Maicol D. Mamani Chalco <mdmch13@gmail.com>
2024-06-14 12:10:28 -0300

    Se modifico y se realizo un demo para prueba

--- db/registro_contable.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE registro_contable;
-
-USE registro_contable;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-);
